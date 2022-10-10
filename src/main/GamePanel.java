02efc9b79d48bbc621eb54c1666404af9c411709
Marcelaun <cellort567@gmail.com>
2022-10-10 12:16:59 -0300

    atualizando gameloop e adicionando classe abstrata para entidades

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -18,23 +18,12 @@ import static utils.Constants.Directions.*;
 public class GamePanel extends JPanel {
 	
 	private MouseInputs mouseInputs;
-	private float xDelta = 100, yDelta = 100;
-	private int frames = 0;
-	private long lastCheck = 0;
-	private BufferedImage img;
-	private BufferedImage[][] animations;
-	private int animationTick, animationIndex, animationSpeed = 15;
-	private int playerAction = IDLE;
-	private int playerDir = -1;
-	private boolean moving = false;
-	 
+	
 	
 	public GamePanel() {
 		
 		mouseInputs = new MouseInputs(this);
 		
-		importImg();
-		loadAnimations();
 		
 		setPanelSize();
 		addKeyListener(new KeyboardInputs(this));
@@ -44,33 +33,7 @@ public class GamePanel extends JPanel {
 		
 	}
 	
-	private void loadAnimations() {
-		animations = new BufferedImage[9][6];
-		
-		for(int j = 0; j < animations.length; j++)
-		
-		for(int i = 0; i < animations[j].length; i++) {
-			animations[j][i] = img.getSubimage(i*64, j*40, 64, 40);
-		}
-		
-	}
-
-	private void importImg() {
-		InputStream is = getClass().getResourceAsStream("/player_sprites.png");
-		
-		try {
-			img = ImageIO.read(is);
-		} catch(IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				is.close();
-			} catch(IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
-	}
+	
 
 	private void setPanelSize() {
 		Dimension size = new Dimension(1280, 800);
@@ -79,72 +42,17 @@ public class GamePanel extends JPanel {
 		
 	}
 
-	public void setDirection(int direction) {
-		this.playerDir = direction;
-		moving = true;
-	}
-	
-	public void setMoving(boolean moving) {
-		this.moving = moving;
-	}
-	
-    private void updateAnimationTick() {
-		
-		animationTick++;
-		if(animationTick >= animationSpeed) {
-			animationTick = 0;
-			animationIndex++;
-			if(animationIndex >= GetSpriteAmount(playerAction)) {
-				animationIndex = 0;
-			}
-		}
-		
-	}
     
-    private void setAnimation() {
-		if(moving) {
-			playerAction = RUNNING;
-			
-		} else {
-			playerAction = IDLE;
-		}
-	}
-    
-    private void updatePos() {
-		if(moving) {
-			switch(playerDir) {
-			case LEFT:
-				xDelta-=5;
-				break;
-			case UP:
-				yDelta-=5;
-				break;
-			case RIGHT:
-				xDelta+=5;
-				break;
-			case DOWN:
-				yDelta+=5;
-				break;
-			
-			}
-		}
+    public void updateGame() {
+    	
 		
-	}
+    }
 
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		updateAnimationTick();
-		
-		setAnimation();
-		updatePos();
-		
-		g.drawImage(animations[playerAction][animationIndex], (int)xDelta, (int)yDelta, 256, 160, null);
-	
-		
 		//g.setColor(Color.decode("#B4392B"));
-		
 		
 		
 		
